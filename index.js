543c083760166a591be04d92ab923f50700f2769
Andy Lowry <andy@andylowry.me.uk>
2016-02-28 00:09:44 +0000

    Add api to allow external access

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var githubprs = require("./githubprs");
 
-setInterval(function () {
-	var prs = githubprs.getPRsMatching('.*');
-	console.dir(prs);
-}, 2000)
+var restify = require('restify');
+
+function respond(req, res, next) {
+  res.send(githubprs.getPRsMatching(req.params.matching || '.*'));
+  next();
+}
+
+var server = restify.createServer();
+server.use(restify.CORS());
+server.use(restify.queryParser());
+server.get('/pullRequests', respond);
+server.head('/pullRequests', respond);
+
+server.listen(4567, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
