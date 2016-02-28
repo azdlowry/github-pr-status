9920371c261a78389ebcea14ce24b21351ffaada
Andy Lowry <andy@andylowry.me.uk>
2016-02-28 00:42:54 +0000

    Use standard port specify var

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ server.use(restify.queryParser());
 server.get('/pullRequests', respond);
 server.head('/pullRequests', respond);
 
-server.listen(process.env.NODE_PORT || 80, function() {
+server.listen(process.env.PORT || 4567, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
